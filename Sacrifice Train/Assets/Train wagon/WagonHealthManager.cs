3b41b157033329bd818a0be1f2752084b0c9445b
Kaptein_Sabeltann <emilnyb@hotmail.com>
2018-08-18 16:48:35 +0200

    finished roof manager and added dmg constant struct

--- Sacrifice Train/Assets/Train wagon/WagonHealthManager.cs
+++ Sacrifice Train/Assets/Train wagon/WagonHealthManager.cs
@@ -9,21 +9,15 @@ public class WagonHealthManager : MonoBehaviour
     public int wagonNumber; // set in inspector for marking purposes
 
     #region CONSTANTS
-    const int MAX_HEALTH = 1000;
-    const int MIN_HEALTH = 0;
+    const float MAX_HEALTH = 1000;
+    const float MIN_HEALTH = 0;
 
-    const int SECONDS_PER_TICK = 5;
-    const int HEALTH_PER_TICK = 10;
-
-    // example dmg values
-    const int RAILGUN_DMG = 3;
-    const int MISSILE_DMG = 30;
-    const int BOMB_DMG = 300;
-    const int SNOWBALL_DMG = 3;
+    const float SECONDS_PER_TICK = 5;
+    const float HEALTH_PER_TICK = 10;
     #endregion
 
     [HideInInspector]
-    public int Health { get; private set; } = MAX_HEALTH;
+    public float Health { get; private set; } = MAX_HEALTH;
 
     [HideInInspector]
     public int RepairWorkers { get; private set; } = 0;
@@ -36,7 +30,7 @@ public class WagonHealthManager : MonoBehaviour
 
     void AddHealth()
     {
-        int sumAddedHealth = RepairWorkers * HEALTH_PER_TICK;
+        float sumAddedHealth = RepairWorkers * HEALTH_PER_TICK;
 
         if (Health + (sumAddedHealth) > MAX_HEALTH)
             Health = MAX_HEALTH;
@@ -44,7 +38,7 @@ public class WagonHealthManager : MonoBehaviour
             Health += sumAddedHealth;
     }
 
-    void TakeDmg(int dmg)
+    void TakeDmg(float dmg)
     {
         if (Health - dmg < MIN_HEALTH)
         {
@@ -65,6 +59,29 @@ public class WagonHealthManager : MonoBehaviour
         enabled = false;
     }
 
+    public void OnCollisionEnter2D(Collision2D c)
+    {
+        switch (c.gameObject.tag)
+        {
+            case "Railgun":
+                TakeDmg(WEAPON_DMG.RAILGUN_DMG);
+                break;
+            case "Missile":
+                TakeDmg(WEAPON_DMG.MISSILE_DMG);
+                break;
+            case "Bomb":
+                TakeDmg(WEAPON_DMG.BOMB_DMG);
+                break;
+            case "Snowball":
+                TakeDmg(WEAPON_DMG.SNOWBALL_DMG);
+                break;
+            default:
+                if (c.gameObject.tag != null && c.gameObject.tag.Length > 0)
+                    Debug.Log("Tag is incorrectly spelled!");
+                break;
+        }
+    }
+
     public void InteractWithWagon(WAGON_INTERACTION interaction)
     {
         switch (interaction)
@@ -76,29 +93,6 @@ public class WagonHealthManager : MonoBehaviour
                 RepairWorkers--;
                 break;
             default:
-                break;
-        }
-    }
-
-    public void OnCollisionEnter(Collision c)
-    {
-        switch (c.gameObject.tag)
-        {
-            case "Railgun":
-                TakeDmg(RAILGUN_DMG);
-                break;
-            case "Missile":
-                TakeDmg(MISSILE_DMG);
-                break;
-            case "Bomb":
-                TakeDmg(BOMB_DMG);
-                break;
-            case "Snowball":
-                TakeDmg(SNOWBALL_DMG);
-                break;
-            default:
-                if (c.gameObject.tag != null && c.gameObject.tag.Length > 0)
-                    Debug.Log("Tag is incorrectly spelled!");
                 break;
         }
     }
