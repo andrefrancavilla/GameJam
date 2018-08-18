60d11fa7c469fdf561c0a8b042603cec8379516a
Kaptein_Sabeltann <emilnyb@hotmail.com>
2018-08-18 20:48:24 +0200

    progress on operations base

---
+++ Sacrifice Train/Assets/OperationsBase/OperationsBaseLauncher.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Collections;
+using UnityEngine;
+
+public class OperationsBaseLauncher : MonoBehaviour
+{
+    public GameObject operationsBase;
+    public Animator anim;
+    public float transitionTime;
+
+    IEnumerator TransitionToBase()
+    {
+        anim.SetTrigger("GoTransition");
+        yield return new WaitForSeconds(transitionTime);
+        operationsBase.SetActive(true);
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+            TransitionToBase();
+    }
+}
--- Sacrifice Train/Assets/OperationsBase/OperationsBaseManager.cs
+++ Sacrifice Train/Assets/OperationsBase/OperationsBaseManager.cs
@@ -1,16 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
+
 using UnityEngine;
 
-public class OperationsBaseManager : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class OperationsBaseManager : MonoBehaviour
+{
+    
 }
