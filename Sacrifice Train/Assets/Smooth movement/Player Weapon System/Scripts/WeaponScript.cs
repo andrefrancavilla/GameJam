8dfb2a0e7d142dba73667c7c0de2eadfa02cade6
andrefrancavilla <andre.francavilla20@gmail.com>
2018-08-19 16:50:59 +0200

    Added burst shooting

--- Sacrifice Train/Assets/Smooth movement/Player Weapon System/Scripts/WeaponScript.cs
+++ Sacrifice Train/Assets/Smooth movement/Player Weapon System/Scripts/WeaponScript.cs
@@ -14,17 +14,25 @@ public class WeaponScript : MonoBehaviour {
     float primaryWeaponBulletSpread;
     float secondaryWeaponBulletSpread;
     float primaryFireT;
-    public float secondaryFireT;
+    float secondaryFireT;
     int primaryWeaponIndex;
     int secondaryWeaponIndex;
     GameObject primaryProjectile;
     GameObject secondaryProjectile;
+    float leftShotCooldown;
+    float rightShotCooldown;
+    float leftShotMem;
+    float rightShotMem;
+    int primaryLeftProjectilesAmount;
+    public int primaryRightProjectilesAmount;
+    int leftShot;
+    public int rightShot;
 
     //Weapon equipment "sensor" system, to configure weapons when equipped
     WEAPON_TYPE previousPrimaryWeapon;
     WEAPON_TYPE previousSecondaryWeapon;
 
-    bool canFire = true;
+    public bool canFire = true;
     
     void Update ()
     {
@@ -35,22 +43,29 @@ public class WeaponScript : MonoBehaviour {
             {
                 case WEAPON_TYPE.RAILGUN:
                     primaryWeaponIndex = 0;
-                    primaryWeaponFireRate = 6;
+                    primaryWeaponFireRate = 9;
                     primaryWeaponBulletSpread = 0.5f;
+                    leftShotCooldown = 0.5f;
+                    primaryLeftProjectilesAmount = 6;
                     break;
                 case WEAPON_TYPE.MISSILE:
                     primaryWeaponIndex = 1;
                     primaryWeaponFireRate = 3;
                     primaryWeaponBulletSpread = 0.33f;
+                    leftShotCooldown = 1.25f;
+                    primaryLeftProjectilesAmount = 3;
                     break;
                 case WEAPON_TYPE.BOMBS:
                     primaryWeaponIndex = 2;
                     primaryWeaponFireRate = 2;
                     primaryWeaponBulletSpread = 0;
+                    leftShotCooldown = 2f;
+                    primaryLeftProjectilesAmount = 3;
                     break;
             }
             primaryProjectile = weaponProjectiles[primaryWeaponIndex];
             previousPrimaryWeapon = currentLeftWeapon;
+            leftShotMem = leftShotCooldown;
         }
         if(previousSecondaryWeapon != currentRightWeapon)
         {
@@ -60,20 +75,27 @@ public class WeaponScript : MonoBehaviour {
                     secondaryWeaponIndex = 0;
                     secondaryWeaponFireRate = 6;
                     secondaryWeaponBulletSpread = 0.5f;
+                    rightShotCooldown = 0.5f;
+                    primaryRightProjectilesAmount = 6;
                     break;
                 case WEAPON_TYPE.MISSILE:
                     secondaryWeaponIndex = 1;
                     secondaryWeaponFireRate = 3;
                     secondaryWeaponBulletSpread = 0.33f;
+                    rightShotCooldown = 1.25f;
+                    primaryRightProjectilesAmount = 3;
                     break;
                 case WEAPON_TYPE.BOMBS:
                     secondaryWeaponIndex = 2;
                     secondaryWeaponFireRate = 2;
                     secondaryWeaponBulletSpread = 0;
+                    rightShotCooldown = 2f;
+                    primaryRightProjectilesAmount = 3;
                     break;
             }
             secondaryProjectile = weaponProjectiles[secondaryWeaponIndex];
             previousSecondaryWeapon = currentRightWeapon;
+            rightShotMem = rightShotCooldown;
         }
 
         //Fire handle
@@ -83,17 +105,39 @@ public class WeaponScript : MonoBehaviour {
         secondaryFireT += Time.deltaTime * secondaryWeaponFireRate;
 
         //Projectile handle
-        if(canFire)
+        if (leftShotCooldown > 0)
+            leftShotCooldown -= Time.deltaTime;
+        if (rightShotCooldown > 0)
+            rightShotCooldown -= Time.deltaTime;
+        if (canFire)
         {
-            if (Input.GetButton("Fire1") && primaryFireT >= 1)
+            if (Input.GetButton("Fire1") && primaryFireT >= 1 && leftShotCooldown <= 0)
             {
-                Instantiate(primaryProjectile, projectileSpawnPositions[primaryWeaponIndex].position + Vector3.up * Random.Range(-primaryWeaponBulletSpread / 2, primaryWeaponBulletSpread / 2), projectileSpawnPositions[primaryWeaponIndex].rotation);
-                primaryFireT = 0;
+                if (leftShot < primaryLeftProjectilesAmount)
+                {
+                    Instantiate(primaryProjectile, projectileSpawnPositions[primaryWeaponIndex].position + Vector3.up * Random.Range(-primaryWeaponBulletSpread / 2, primaryWeaponBulletSpread / 2), projectileSpawnPositions[primaryWeaponIndex].rotation);
+                    primaryFireT = 0;
+                    leftShot++;
+                }
+                else
+                {
+                    leftShot = 0;
+                    leftShotCooldown = leftShotMem;
+                }
             }
-            if (Input.GetButton("Fire2") && secondaryFireT >= 1)
+            if (Input.GetButton("Fire2") && secondaryFireT >= 1 && leftShotCooldown <= 0)
             {
-                Instantiate(secondaryProjectile, projectileSpawnPositions[secondaryWeaponIndex].position + Vector3.up * Random.Range(-secondaryWeaponBulletSpread / 2, secondaryWeaponBulletSpread / 2), projectileSpawnPositions[secondaryWeaponIndex].rotation);
-                secondaryFireT = 0;
+                if(rightShot < primaryRightProjectilesAmount)
+                {
+                    Instantiate(secondaryProjectile, projectileSpawnPositions[secondaryWeaponIndex].position + Vector3.up * Random.Range(-secondaryWeaponBulletSpread / 2, secondaryWeaponBulletSpread / 2), projectileSpawnPositions[secondaryWeaponIndex].rotation);
+                    secondaryFireT = 0;
+                    rightShot++;
+                }
+                else
+                {
+                    rightShot = 0;
+                    rightShotCooldown = rightShotMem;
+                }
             }
         }
     }
