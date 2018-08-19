b4d21b9783754b5ac51c862061511d7d5e67dcb0
Kaptein_Sabeltann <emilnyb@hotmail.com>
2018-08-19 12:41:10 +0200

    started on prisoner script

---
+++ Sacrifice Train/Assets/OperationsBase/ReleasePrisoners.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReleasePrisoners : MonoBehaviour
+{
+    public Button btn;
+    public GameObject prisonerPrefab;
+    public Transform prisonerSpawnPoint;
+
+    public PlayerController player;
+    public float prisonerSpawnRate = 0.75f;
+
+    void Start()
+    {
+        btn.onClick.AddListener(() => StartCoroutine(Release()));
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+            Release();
+    }
+
+    IEnumerator Release()
+    {
+        for (int i = 0; i < player.prisonersSaved; i++)
+        {
+            var prisoner = Instantiate(prisonerPrefab, prisonerSpawnPoint.position, Quaternion.identity);
+            prisoner.GetComponent<Prisoner>().RunAcrossScreen();
+
+            yield return new WaitForSeconds(prisonerSpawnRate);
+        }
+        
+    }
+}
